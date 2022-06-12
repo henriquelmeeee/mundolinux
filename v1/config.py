6ca04ceff58e34b5a38f13d69b8d76a10cd1ff01
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-06-11 21:14:02 -0300

    first code

---
+++ v1/config.py
@@ -0,0 +1,55 @@
+token = "" # bearer_token
+
+words = [
+    '#mundolinux',
+    'linux',
+    'mundolinux',
+    'linux', 
+    'Linux',
+    'xfce',
+    'gnome',
+]
+
+words_not_accepted = [ #remove specific words and letters to filter with the best result, especially in Portuguese
+    'if',
+    'all',
+    'can\'t',
+    'can',
+    'cannot',
+    'or',
+    'the',
+    'using',
+    'on',
+    'gift',
+    'gifts',
+    'giftcard',
+    'giftcards',
+    'secret',
+    'egg',
+    'easter',
+    'easteregg',
+    'machine',
+    'value',
+    'how',
+    '&'
+    'к',
+    'ь',
+    'т',
+    'я',
+    'п'
+    'は',
+    'ダ',
+    'す',
+    '♪',
+    '本',
+    'の',
+    'べ',
+    'す',
+    '本',
+    'マ'
+]
+
+api_key = ''
+api_secret = ''
+access_token = ''
+access_token_secret = ''
