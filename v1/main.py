f51db64785775cab3924daae4f1fbaf7ebf049a7
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-06-12 14:25:41 -0300

    add logs system in background

--- v1/main.py
+++ v1/main.py
@@ -1,6 +1,6 @@
-import tweepy, time, os, random
+import tweepy, time, os, random, threading
 from datetime import datetime
-import config, algorithm
+import config, algorithm, logs
 
 client = tweepy.Client(config.token, config.api_key, config.api_secret, config.access_token, config.access_token_secret)
 
@@ -21,18 +21,19 @@ class MyStream(tweepy.StreamingClient):
 
     def on_connect(self):
         print(
-            f"--------------------\nBot iniciado com sucesso!\n--------------------"
+            f"Bot iniciado"
         )
 
     def on_tweet(self, tweet):
         try:
-            if '#mundolinux' in tweet.text and ' ' in tweet.text:
+            if '#mundolinux' in tweet.text or '@mundolinuxbot' in tweet.text and ' ' in tweet.text:
                 retweet(tweet.id)
             else:
-                if algorithm.check(tweet.text) and tweet.referenced_tweets is None and retweet(tweet.id):
+                if algorithm.check(tweet.text) and retweet(tweet.id):
                     print(f'{datetime.now()} | Tweet de ID {tweet.id} retweetado com sucesso!')
                     if int(datetime.now().hour) > 7 and not int(datetime.now().hour) < 21:
                         time.sleep(random.randint(10, 20))
+                    config.retweeted_today += 1
             time.sleep(random.randint(20, 60))
         except Exception as error:
             print('--------------------' + str(error) + '--------------------')
@@ -53,8 +54,13 @@ if add_rules:
             )
     except Exception as erro:
         print(f"Um erro ocorreu!\n'{str(erro)}'\nO bot irá continuar a funcionar mesmo assim.")
+os.system('clear')
 
-os.system('clear && rm -rf __pycache__ && echo "Iniciando bot..."')
+print('Iniciando logs...')
+os.system('rm -rf __pycache__ && echo "Iniciando bot..."')
+
+log = threading.Thread(name='logs', target=logs.logs)
+log.start() # Start logs system in background because because it will be running in loop
 
 auth = tweepy.OAuthHandler(config.api_key, config.api_secret)
 auth.set_access_token(config.access_token, config.access_token_secret)
