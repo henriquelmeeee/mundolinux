820fe9f5ed5dcd4cfd51f3969f8f5d9ea7cd9c6e
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-06-12 00:14:09 -0300

    Create algorithm.py

---
+++ v1/algorithm.py
@@ -0,0 +1,56 @@
+import config
+
+def is_alphanum(text : str):
+    letters = [
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+        'u', 'v', 'w', 'x', 'y', 'z'
+    ]
+    for letter in text.lower():
+        if not letter in letters:
+            return False
+    return True
+
+def check_stage_three(text):
+    if text == 'linux' or text == 'xfce' or text == 'gnome' or text == 'mundolinux':
+        return False # remove tweets very simple because even involving linux, they do not contribute anything
+    if not ' ' in text:
+        return False # it will probably be a link
+    word = ''
+    for letter in text: # sometimes some tweets get passed to the bot even though it's not in the word list, so this script will check that
+        if letter == ' ':
+            if word in config.words:
+                return True
+            else:
+                word = ''
+        else:
+            word += letter
+    return True 
+
+def check_stage_two(text):
+    for letter in text:
+        if letter in config.words_not_accepted:
+            return False
+    return True
+
+def check_stage_one(text):
+    hashtag_number = 0
+    word = ''
+    for letter in text:
+        if ' ' in letter:
+            if word in config.words_not_accepted:
+                return False
+            word = ''
+        else:
+            word += letter
+        if '#' in letter:
+            hashtag_number += 1
+    if hashtag_number > 3:
+        return False
+    return True if not word.replace(' ', '') in config.words_not_accepted else False
+
+
+def check(text : str):
+    text = text.lower()
+    if check_stage_one(text) and check_stage_two(text) and check_stage_three(text):
+        return True
+    return False
