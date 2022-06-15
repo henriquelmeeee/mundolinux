02d3c04748e94c8dd81f8e9bfbb42e0c3bab902c
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-06-15 12:19:23 -0300

    update variables

--- v1/logs.py
+++ v1/logs.py
@@ -5,17 +5,25 @@ from datetime import datetime
 day = int(datetime.now().day)
 month = int(datetime.now().month)
 year = int(datetime.now().year)
+hour = int(datetime.now().hour)
+
+def update_variables():
+    day = int(datetime.now().day); month = int(datetime.now().month); year = int(datetime.now().year); hour = int(datetime.now().hour)
+    return True
 
 def send_log():
     f = open('logs.txt', 'a')
-    f.write(f'Dia {day}/{month}/{year}: ' + str(config.retweeted_today) + '\n')
+    f.write(f'Dia {day}/{month}/{year}: ' + str(config.retweeted_today) + ' tweets retweetados\n')
     f.close
+    update_variables()
     config.retweeted_today = 0
     print(f'{datetime.now()} | Logs do dia {day} registradas com sucesso!')
     return True
 
 def logs():
     print('Sistema de logs iniciado')
+    if hour == 0:
+        time.sleep(3600)
     while True:
         if int(datetime.now().hour) == 0:
             print(f'{datetime.now()} | Registrando logs do dia {day}...')
