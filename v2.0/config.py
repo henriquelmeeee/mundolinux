21a15dbe46b931979688981ef02d4f86b36e369f
Henrique <86432400+henriquelmeeee@users.noreply.github.com>
2022-07-02 15:30:39 -0300

    Create config.py

---
+++ v2.0/config.py
@@ -0,0 +1,47 @@
+token = "" # bearer_token
+
+retweeted_today = 0
+
+words = [
+    '#mundolinux',
+    'linux',
+    'mundolinux',
+    '@mundolinuxbot',
+    'xfce',
+    'ubuntu'
+]
+
+words_not_accepted = [ #remove specific words and letters to filter with the best result, especially in Portuguese
+    '&',
+    'к',
+    'ь',
+    'т',
+    'я',
+    'п'
+    'は',
+    'ダ',
+    'す',
+    '♪',
+    '本',
+    'の',
+    'べ',
+    'す',
+    '本',
+    'マ',
+    '在',
+    '应',
+    '用',
+    '糊',
+    '信',
+    'リ',
+    'ス',
+    'を',
+    'ー',
+    'न',
+    'ज'
+]
+
+api_key = ''
+api_secret = ''
+access_token = ''
+access_token_secret = ''
